5c76a6c9bcc8663f7106ff7f83d03aff6d43f78d
Anthony Fakhoury <anthony.fakhoury@mail.utoronto.ca>
2021-03-27 11:00:26 -0400

    started lab4: packet.h done client.c in progress

---
+++ client.c
@@ -0,0 +1,283 @@
+/*
+	client.c 
+	Made by Anthony Fakhoury and Romil Jain
+	Inspired by snipets of code from Beej's Guide to Network Programming
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+#include <pthread.h>
+
+#include "packet.h"
+
+//forward declarations
+void askInput(char buf[MAXBUFLEN], char command[MAX_NAME], char arg1[MAX_NAME], char arg2[MAX_NAME],
+			  char arg3[MAX_NAME], char arg4[MAX_NAME], char extra[MAX_NAME]);
+void login(char arg1[MAX_NAME], char arg2[MAX_NAME], char arg3[MAX_NAME], char arg4[MAX_NAME], int *sockfd, pthread_t *clientThread);
+void logout(int *sockfd, pthread_t *clientThread);
+void joinsession(char arg1[MAX_NAME], &sockfd);
+
+
+int main(int argc, char *argv[])
+{
+	int sockfd;
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	int numbytes;
+	char buf[MAXBUFLEN];
+	char s[INET_ADDRSTRLEN];
+	char command[MAX_NAME];
+	char arg1[MAX_NAME];
+	char arg2[MAX_NAME];
+	char arg3[MAX_NAME];
+	char arg4[MAX_NAME];
+	char extra[MAX_NAME];
+	pthread_t clientThread;
+
+
+	if (argc != 1) {
+		fprintf(stderr,"usage\n");
+		exit(1);
+	}
+
+	while(1){
+		//ask the user to input a message 
+		ask_input:
+		askInput(buf, command, arg1, arg2, arg3, arg4, extra);
+		//check input length
+		if (command[0] == '\0') {
+			perror("too few arguments or too long\n");
+			goto ask_input;
+		} else if (extra[0] != '\0') {
+			perror("too many arguments\n");
+			goto ask_input;
+		}
+
+		//check command
+		switch (command)
+		{
+		case "/login":
+			//check input length
+			if (arg1[0] == '\0' || arg2[0] == '\0' || arg3[0] == '\0' || arg4[0] == '\0') {
+				perror("too few arguments or too long\n");
+				goto ask_input;
+			} else if (extra[0] != '\0') {
+				perror("too many arguments\n");
+				goto ask_input;
+			}
+			login(arg1, arg2, arg3, arg4, &sockfd, &clientThread);
+			break;
+		case "/logout":
+			if (arg1[0] != '\0') {
+				perror("too many arguments\n");
+				goto ask_input;
+			}
+			logout(&sockfd, &clientThread);
+			break;
+		case "/joinsession":
+			if (arg1[0] == '\0') {
+				perror("too few arguments or too long\n");
+				goto ask_input;
+			} else if (arg2[0] != '\0') {
+				perror("too many arguments\n");
+				goto ask_input;
+			}
+			joinsession(arg1, &sockfd);
+			break;
+		case "/leavesession":
+			if (arg1[0] != '\0') {
+				perror("too many arguments\n");
+				goto ask_input;
+			}
+			leavesession(&sockfd);
+			break;
+		case "/createsession":
+			if (arg1[0] != '\0') {
+				perror("too many arguments\n");
+				goto ask_input;
+			}
+			createsession(arg1, &sockfd);
+			break;
+		case "/list":
+			if (arg1[0] != '\0') {
+				perror("too many arguments\n");
+				goto ask_input;
+			}
+			list(&sockfd);
+			break;
+		case "/quit":
+			if (arg1[0] != '\0') {
+				perror("too many arguments\n");
+				goto ask_input;
+			}
+			quit(&sockfd, &clientThread);
+			fprintf(stdout, "EXIT\n");
+			return 0;
+			break;
+		default: //command is the message to be sent
+			if (arg1[0] != '\0') {
+				perror("too many arguments\n");
+				goto ask_input;
+			}
+			break;
+		}
+	}
+	fprintf(stdout, "EXIT\n");
+	return 0;
+}
+
+/*  
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET; // set to AF_INET6 to use IPv6
+	hints.ai_socktype = SOCK_DGRAM;
+
+	if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and make a socket
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("talker: socket");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "talker: failed to create socket\n");
+		return 2;
+	}
+
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+				s, sizeof s);
+	printf("talker: connecting to %s\n", s);
+
+	freeaddrinfo(servinfo);
+*/
+
+void askInput(char buf[MAXBUFLEN], char command[MAX_NAME], char arg1[MAX_NAME], char arg2[MAX_NAME],
+			  char arg3[MAX_NAME], char arg4[MAX_NAME], char extra[MAX_NAME]){
+
+	printf("User: insert command\n");
+	memset(buf, 0, MAXBUFLEN); // first empty the buffer
+	fgets(buf, MAXBUFLEN, stdin);
+	fflush(stdin);
+
+	//extract user input (instead of using scanf as requested by TA)
+	int count = 0;
+	while (buf[count]==' ') {
+		if (count >= MAXBUFLEN) {
+			break;
+		}
+		count++;
+	}
+	int index = 0;
+	while (buf[count]==' ') {
+		if (count >= MAXBUFLEN) {
+			break;
+		}
+		count++;
+	}
+	while (buf[count]!=' ') {
+		if (buf[count]=='\0' || buf[count]=='\n' || count >= MAXBUFLEN) {
+			break;
+		}
+		command[index] = buf[count];
+		count++;
+		index++;
+	}
+	command[index] = '\0';
+	index = 0;
+	while (buf[count]!=' ') {
+		if (buf[count]=='\0' || buf[count]=='\n' || count >= MAXBUFLEN) {
+			break;
+		}
+		arg1[index] = buf[count];
+		count++;
+		index++;
+	}
+	arg1[index] = '\0';
+	index = 0;
+	while (buf[count]==' ') {
+		if (count >= MAXBUFLEN) {
+			break;
+		}
+		count++;
+	}
+	while (buf[count]!=' ') {
+		if (buf[count]=='\0' || buf[count]=='\n' || count >= MAXBUFLEN) {
+			break;
+		}
+		arg2[index] = buf[count];
+		count++;
+		index++;
+	}
+	arg2[index] = '\0';
+	index = 0;
+	while (buf[count]==' ') {
+		if (count >= MAXBUFLEN) {
+			break;
+		}
+		count++;
+	}
+	while (buf[count]!=' ') {
+		if (buf[count]=='\0' || buf[count]=='\n' || count >= MAXBUFLEN) {
+			break;
+		}
+		arg3[index] = buf[count];
+		count++;
+		index++;
+	}
+	arg3[index] = '\0';
+	index = 0;
+	while (buf[count]==' ') {
+		if (count >= MAXBUFLEN) {
+			break;
+		}
+		count++;
+	}
+	while (buf[count]!=' ') {
+		if (buf[count]=='\0' || buf[count]=='\n' || count >= MAXBUFLEN) {
+			break;
+		}
+		arg4[index] = buf[count];
+		count++;
+		index++;
+	}
+	arg4[index] = '\0';
+	index = 0;
+	while (buf[count]==' ') {
+		if (count >= MAXBUFLEN) {
+			break;
+		}
+		count++;
+	}
+	while (buf[count]!=' ') {
+		if (buf[count]=='\0' || buf[count]=='\n' || count >= MAXBUFLEN) {
+			break;
+		}
+		extra[index] = buf[count];
+		count++;
+		index++;
+	}
+	extra[index] = '\0';
+	while (buf[count]==' ') {
+		if (buf[count]=='\0' || count >= MAXBUFLEN) {
+			break;
+		}
+		count++;
+	}
+}
